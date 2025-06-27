cb1e61a8f40f8f38e0a154cc4ca8f83cfd8d96e2
ean <52894214+ean161@users.noreply.github.com>
2025-06-27 12:31:12 +0700

    add block player move on not logged in

--- src/main/java/vn/ean/a2nuth/App.java
+++ src/main/java/vn/ean/a2nuth/App.java
@@ -1,10 +1,20 @@
 package vn.ean.a2nuth;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.entity.Player;
+import org.bukkit.Location;
 
 public class App extends JavaPlugin implements Listener {
+    List<String> logged = new ArrayList<>();
+
     @Override
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(this, this);
@@ -14,5 +24,21 @@ public class App extends JavaPlugin implements Listener {
     @Override
     public void onDisable() {
         getLogger().info("A2nuth disabled");
+    }
+
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent event) {
+        Player player = event.getPlayer();
+        if (logged.contains(player.getName()))
+            return;
+
+        sendMessage(player, "Bạn chưa đăng nhập, không thể di chuyển");
+        event.setCancelled(true);
+    }
+
+    public void sendMessage(Player player, String message) {
+        player.sendMessage(String.format("[A2nuth] %s", message));
+    
+        return;
     }
 }
