43a05d890eb287fe5de9492ace938e25634662c9
ean <52894214+ean161@users.noreply.github.com>
2025-06-27 12:50:36 +0700

    add database controll

--- src/main/java/vn/ean/a2nuth/App.java
+++ src/main/java/vn/ean/a2nuth/App.java
@@ -1,22 +1,33 @@
 package vn.ean.a2nuth;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import io.papermc.paper.event.player.AsyncChatEvent;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
+
 import org.bukkit.entity.Player;
-import org.bukkit.Location;
 
 public class App extends JavaPlugin implements Listener {
     List<String> logged = new ArrayList<>();
+    File configFile;
+
+    FileConfiguration config;
 
     @Override
     public void onEnable() {
+        setupConfig();
         Bukkit.getPluginManager().registerEvents(this, this);
         getLogger().info("A2nuth enabled");
     }
@@ -26,14 +37,52 @@ public class App extends JavaPlugin implements Listener {
         getLogger().info("A2nuth disabled");
     }
 
+    public void setupConfig() {
+        configFile = new File(getDataFolder(), "data.yml");
+
+        if (!configFile.exists()) {
+            configFile.getParentFile().mkdirs();
+            try {
+                configFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        config = YamlConfiguration.loadConfiguration(configFile);
+    }
+
+    public void saveConfig() {
+        try {
+            config.save(configFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void authRequest(Player player) {
+
+    }
+
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent event) {
         Player player = event.getPlayer();
         if (logged.contains(player.getName()))
             return;
 
-        sendMessage(player, "Bạn chưa đăng nhập, không thể di chuyển");
+        authRequest(player);
         event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onChat(AsyncChatEvent event) {
+        String message = PlainTextComponentSerializer.plainText().serialize(event.message());
+        Player player = event.getPlayer();
+
+        config.set(player.getName(), message);
+        saveConfig();
+
+        sendMessage(player, message);
     }
 
     public void sendMessage(Player player, String message) {
