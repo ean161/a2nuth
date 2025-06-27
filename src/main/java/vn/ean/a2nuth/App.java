b17c9e307f452fe58648d6ab65e242bca8c1b113
ean <52894214+ean161@users.noreply.github.com>
2025-06-27 13:06:38 +0700

    add login/register logic

--- src/main/java/vn/ean/a2nuth/App.java
+++ src/main/java/vn/ean/a2nuth/App.java
@@ -10,6 +10,7 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -23,7 +24,7 @@ public class App extends JavaPlugin implements Listener {
     List<String> logged = new ArrayList<>();
     File configFile;
 
-    FileConfiguration config;
+    FileConfiguration db;
 
     @Override
     public void onEnable() {
@@ -49,19 +50,56 @@ public class App extends JavaPlugin implements Listener {
             }
         }
 
-        config = YamlConfiguration.loadConfiguration(configFile);
+        db = YamlConfiguration.loadConfiguration(configFile);
     }
 
     public void saveConfig() {
         try {
-            config.save(configFile);
+            db.save(configFile);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public void authRequest(Player player) {
+        sendMessage(player, "Hãy gửi mật khẩu vào khung chat để đăng " + (db.contains(player.getName()) ? "nhập" : "ký"));
+    }
 
+    public void authHandler(Player player, String password) {
+        String prefixType = "Đăng nhập";
+        password = password.trim();
+
+        if (db.contains(player.getName())) {
+            if (!db.getString(player.getName()).equals(password)) {
+                sendMessage(player, "Sai mật khẩu, hãy thử lại");
+                return;
+            }
+        } else {
+            prefixType = "Đăng ký";
+            if (password.length() < 6) {
+                sendMessage(player, "Mật khẩu quá yếu, hãy thử lại");
+                return;
+            }
+
+            db.set(player.getName(), password);
+        }
+
+        logged.add(player.getName());
+        sendMessage(player, String.format("%s thành công, chúc bạn chơi vui vẻ", prefixType));
+    }
+
+    @EventHandler
+    public void PlayerJoinEvent(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+
+        authRequest(player);
+    }
+
+    @EventHandler
+    public void PlayerQuitEvent(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+
+        logged.remove(player.getName());
     }
 
     @EventHandler
@@ -78,11 +116,11 @@ public class App extends JavaPlugin implements Listener {
     public void onChat(AsyncChatEvent event) {
         String message = PlainTextComponentSerializer.plainText().serialize(event.message());
         Player player = event.getPlayer();
+        if (logged.contains(player.getName()))
+            return;
 
-        config.set(player.getName(), message);
-        saveConfig();
-
-        sendMessage(player, message);
+        authHandler(player, message);
+        event.setCancelled(true);
     }
 
     public void sendMessage(Player player, String message) {
